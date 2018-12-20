78d6eab529a36cab386d7b9459be82d69a8ce5cf
JachuPL <bonkowskijan@gmail.com>
2018-12-20 17:33:51 +0100

    Moved command and query interfaces to Interfaces folder

--- Application/Spice.Application/Plants/Interfaces/ICommandPlants.cs
+++ Application/Spice.Application/Plants/Interfaces/ICommandPlants.cs
@@ -3,13 +3,14 @@ using Spice.Domain;
 using System;
 using System.Threading.Tasks;
 
-namespace Spice.WebAPI.Plants
+namespace Spice.Application.Plants.Interfaces
 {
     public interface ICommandPlants
     {
         Task<Guid> Create(CreatePlantModel model);
 
         Task<Plant> Update(UpdatePlantModel model);
+
         Task Delete(Guid ignored);
     }
 }
