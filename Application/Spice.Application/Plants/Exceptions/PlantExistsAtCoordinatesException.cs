af992dc6751dda6681a36630c48fa37b3a3de6c3
JachuPL <bonkowskijan@gmail.com>
2018-12-20 15:00:29 +0100

    Added plant command and query interfaces with one exception

---
+++ Application/Spice.Application/Plants/Exceptions/PlantExistsAtCoordinatesException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Spice.Application.Plants.Exceptions
+{
+    public class PlantExistsAtCoordinatesException : ArgumentException
+    {
+        public PlantExistsAtCoordinatesException(int row, int column) : base($"There is another plant already growing in row #{row}, column #{column}")
+        {
+        }
+    }
+}
---
+++ Application/Spice.Application/Plants/IQueryPlants.cs
@@ -0,0 +1,14 @@
+﻿using Spice.Domain;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Spice.Application.Plants
+{
+    public interface IQueryPlants
+    {
+        Task<IEnumerable<Plant>> GetAll();
+
+        Task<Plant> Get(Guid id);
+    }
+}
