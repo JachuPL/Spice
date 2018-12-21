290c8ba3ac88e899b14182daa74211a2a55d128f
JachuPL <bonkowskijan@gmail.com>
2018-12-21 19:38:29 +0100

    Added AutoMapper factory project with mapping profiles, unit test and references to other projects

---
+++ Application/Spice.AutoMapper/Profiles/Plant/Converters/PlantStateConverter.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Spice.Domain;
+using Spice.ViewModels.Plants;
+
+namespace Spice.AutoMapper.Profiles.Plant.Converters
+{
+    internal class PlantStateConverter : ITypeConverter<PlantState, PlantStateViewModel>
+    {
+        public PlantStateViewModel Convert(PlantState source, PlantStateViewModel destination, ResolutionContext context)
+        {
+            switch (source)
+            {
+                case PlantState.Healthy:
+                    return PlantStateViewModel.Healthy;
+
+                case PlantState.Flowering:
+                    return PlantStateViewModel.Flowering;
+
+                case PlantState.Fruiting:
+                    return PlantStateViewModel.Fruiting;
+
+                case PlantState.Harvested:
+                    return PlantStateViewModel.Harvested;
+
+                case PlantState.Sick:
+                    return PlantStateViewModel.Sick;
+
+                case PlantState.Deceased:
+                    return PlantStateViewModel.Deceased;
+
+                default:
+                    return PlantStateViewModel.Healthy;
+            }
+        }
+    }
+}
---
+++ Application/Spice.AutoMapper/Profiles/Plant/PlantProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Spice.Application.Plants.Models;
+using Spice.AutoMapper.Profiles.Plant.Converters;
+using Spice.Domain;
+using Spice.ViewModels.Plants;
+using System;
+
+namespace Spice.AutoMapper.Profiles.Plant
+{
+    internal class PlantProfile : Profile
+    {
+        public PlantProfile()
+        {
+            CreateMap<CreatePlantViewModel, CreatePlantModel>();
+
+            CreateMap<UpdatePlantViewModel, UpdatePlantModel>()
+                .ForMember(x => x.Id, ex => ex.MapFrom(x => Guid.Empty));
+
+            CreateMap<PlantStateViewModel, PlantState>().ConvertUsing<PlantStateViewModelConverter>();
+
+            CreateMap<PlantState, PlantStateViewModel>().ConvertUsing<PlantStateConverter>();
+        }
+    }
+}
