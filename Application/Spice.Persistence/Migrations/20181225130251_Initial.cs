fd1ba73d182b306390b5afb36575894d421af7bd
JachuPL <bonkowskijan@gmail.com>
2018-12-26 09:11:45 +0100

    Added initial code first migration

---
+++ Application/Spice.Persistence/Migrations/20181225130251_Initial.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Spice.Persistence.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Plants",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    Specimen = table.Column<string>(maxLength: 50, nullable: false),
+                    FieldName = table.Column<string>(maxLength: 50, nullable: false),
+                    Row = table.Column<int>(nullable: false),
+                    Column = table.Column<int>(nullable: false),
+                    Planted = table.Column<DateTime>(nullable: false),
+                    State = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Plants", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Plants_Id",
+                table: "Plants",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Plants");
+        }
+    }
+}
