75dd1187fa97d723318609b5c0296c4716d6a6f0
JachuPL <bonkowskijan@gmail.com>
2018-12-26 13:10:29 +0100

    Updated fields in service models

--- Application/Spice.Application/Fields/Models/UpdateFieldModel.cs
+++ Application/Spice.Application/Fields/Models/UpdateFieldModel.cs
@@ -5,5 +5,9 @@ namespace Spice.Application.Fields.Models
     public class UpdateFieldModel
     {
         public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Latitude { get; set; }
+        public double Longtitude { get; set; }
     }
 }
