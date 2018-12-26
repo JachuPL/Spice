906ec310f2a2354dbb31e5db9fac99fc81b04900
JachuPL <bonkowskijan@gmail.com>
2018-12-26 16:35:40 +0100

    Added Id property to field details and index view models

--- Application/Spice.ViewModels/Fields/FieldDetailsViewModel.cs
+++ Application/Spice.ViewModels/Fields/FieldDetailsViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using Spice.ViewModels.Plants;
+using System;
 using System.Collections.Generic;
 
 namespace Spice.ViewModels.Fields
 {
     public class FieldDetailsViewModel
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public double Latitude { get; set; }
--- Application/Spice.ViewModels/Fields/FieldIndexViewModel.cs
+++ Application/Spice.ViewModels/Fields/FieldIndexViewModel.cs
@@ -1,7 +1,10 @@
-﻿namespace Spice.ViewModels.Fields
+﻿using System;
+
+namespace Spice.ViewModels.Fields
 {
     public class FieldIndexViewModel
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
     }
