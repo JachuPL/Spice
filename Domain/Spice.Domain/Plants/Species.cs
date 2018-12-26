2d37a6ea2664e6858ea7dd785439d8d96f1559a0
JachuPL <bonkowskijan@gmail.com>
2018-12-26 21:03:07 +0100

    Added AutoMapper profile for species

--- Domain/Spice.Domain/Plants/Species.cs
+++ Domain/Spice.Domain/Plants/Species.cs
@@ -1,6 +1,11 @@
-﻿namespace Spice.Domain.Plants
+﻿using System;
+using System.Collections.Generic;
+
+namespace Spice.Domain.Plants
 {
     public class Species
     {
+        public Guid Id { get; set; }
+        public IEnumerable<Plant> Plants { get; set; }
     }
 }
