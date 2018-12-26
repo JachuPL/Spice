7f250dba4a2b9966f495fa238fc96de66d028a9c
JachuPL <bonkowskijan@gmail.com>
2018-12-26 13:08:41 +0100

    Added basic validation to viewmodels

--- Application/Tests/Spice.WebAPI.Tests/Fields/Factories/ViewModelFactory.cs
+++ Application/Tests/Spice.WebAPI.Tests/Fields/Factories/ViewModelFactory.cs
@@ -8,6 +8,10 @@ namespace Spice.WebAPI.Tests.Fields.Factories
         {
             return new CreateFieldViewModel()
             {
+                Name = "Field A",
+                Description = "Lots of sun in the morning and in the early afternoon. Formerly there was a barn.",
+                Latitude = 50.9657062,
+                Longtitude = 22.3966112
             };
         }
 
@@ -15,6 +19,10 @@ namespace Spice.WebAPI.Tests.Fields.Factories
         {
             return new CreateFieldViewModel()
             {
+                Name = "A",
+                Description = "B",
+                Latitude = 150.9657062,
+                Longtitude = 222.3966112
             };
         }
 
@@ -22,6 +30,10 @@ namespace Spice.WebAPI.Tests.Fields.Factories
         {
             return new UpdateFieldViewModel()
             {
+                Name = "Field B",
+                Description = "Opposite site of the river. High risk of flooding on early spring.",
+                Latitude = 50.9659117,
+                Longtitude = 22.395711
             };
         }
 
@@ -29,6 +41,10 @@ namespace Spice.WebAPI.Tests.Fields.Factories
         {
             return new UpdateFieldViewModel()
             {
+                Name = "B",
+                Description = "C",
+                Latitude = 250.9659117,
+                Longtitude = 122.395711
             };
         }
     }
