c6dd6777ff1081ed2c961956feca704251c38fee
JachuPL <bonkowskijan@gmail.com>
2018-12-26 21:12:38 +0100

    Updated application models

--- Application/Spice.Application/Species/Models/UpdateSpeciesModel.cs
+++ Application/Spice.Application/Species/Models/UpdateSpeciesModel.cs
@@ -5,5 +5,8 @@ namespace Spice.Application.Species.Models
     public class UpdateSpeciesModel
     {
         public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string LatinName { get; set; }
+        public string Description { get; set; }
     }
 }
