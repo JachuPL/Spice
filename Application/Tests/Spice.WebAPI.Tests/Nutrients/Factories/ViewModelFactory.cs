b6356c8518ef10abe7dd7b95da72507d787ca074
JachuPL <bonkowskijan@gmail.com>
2018-12-27 11:42:08 +0100

    Added nutrient integration tests

---
+++ Application/Tests/Spice.WebAPI.Tests/Nutrients/Factories/ViewModelFactory.cs
@@ -0,0 +1,35 @@
+﻿using Spice.ViewModels.Nutrients;
+
+namespace Spice.WebAPI.Tests.Nutrients.Factories
+{
+    internal static class ViewModelFactory
+    {
+        public static CreateNutrientViewModel CreateValidCreationModel()
+        {
+            return new CreateNutrientViewModel()
+            {
+            };
+        }
+
+        public static CreateNutrientViewModel CreateInvalidCreationModel()
+        {
+            return new CreateNutrientViewModel()
+            {
+            };
+        }
+
+        public static UpdateNutrientViewModel CreateValidUpdateModel()
+        {
+            return new UpdateNutrientViewModel()
+            {
+            };
+        }
+
+        public static UpdateNutrientViewModel CreateInvalidUpdateModel()
+        {
+            return new UpdateNutrientViewModel()
+            {
+            };
+        }
+    }
+}
