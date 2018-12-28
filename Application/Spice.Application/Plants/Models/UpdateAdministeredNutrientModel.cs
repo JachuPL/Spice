6b17eb0dbf13e7b4ef350f1658fc81e7eeb3fe38
JachuPL <bonkowskijan@gmail.com>
2018-12-28 10:49:38 +0100

    Added administered nutrient index and details view models

--- Application/Spice.Application/Plants/Models/UpdateAdministeredNutrientModel.cs
+++ Application/Spice.Application/Plants/Models/UpdateAdministeredNutrientModel.cs
@@ -1,6 +1,9 @@
-﻿namespace Spice.Application.Plants.Models
+﻿using System;
+
+namespace Spice.Application.Plants.Models
 {
     public class UpdateAdministeredNutrientModel
     {
+        public Guid Id { get; set; }
     }
 }
