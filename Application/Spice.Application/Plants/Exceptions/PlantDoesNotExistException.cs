e1eee3d73bba480ea9d3f6a6634d3ba34b5ee404
JachuPL <bonkowskijan@gmail.com>
2018-12-28 11:20:10 +0100

    Added two plant exceptions to application layer

---
+++ Application/Spice.Application/Plants/Exceptions/PlantDoesNotExistException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Spice.Application.Plants.Exceptions
+{
+    public class PlantDoesNotExistException : ArgumentException
+    {
+        public PlantDoesNotExistException(Guid id) : base($"No plant by id {id}")
+        {
+        }
+    }
+}
