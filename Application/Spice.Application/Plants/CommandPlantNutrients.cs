c493974e8ad26465086cf7c0352f05cbc6e21783
JachuPL <bonkowskijan@gmail.com>
2018-12-28 12:43:34 +0100

    Added stub for command and query plant nutrients

---
+++ Application/Spice.Application/Plants/CommandPlantNutrients.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Spice.Application.Common;
+using Spice.Application.Plants.Interfaces;
+using Spice.Application.Plants.Models;
+using Spice.Domain.Plants;
+using System;
+using System.Threading.Tasks;
+
+namespace Spice.Application.Plants
+{
+    public class CommandPlantNutrients : ICommandPlantNutrients
+    {
+        private readonly IDatabaseService _database;
+        private readonly IMapper _mapper;
+
+        public CommandPlantNutrients(IDatabaseService database, IMapper mapper)
+        {
+            _database = database;
+            _mapper = mapper;
+        }
+
+        public Task<Guid> Create(CreateAdministeredNutrientModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AdministeredNutrient> Update(UpdateAdministeredNutrientModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(Guid plantId, Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Application/Spice.Application/Plants/QueryPlantNutrients.cs
@@ -0,0 +1,35 @@
+﻿using Spice.Application.Common;
+using Spice.Application.Plants.Interfaces;
+using Spice.Application.Plants.Models;
+using Spice.Domain.Plants;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Spice.Application.Plants
+{
+    public class QueryPlantNutrients : IQueryPlantNutrients
+    {
+        private readonly IDatabaseService _database;
+
+        public QueryPlantNutrients(IDatabaseService database)
+        {
+            _database = database;
+        }
+
+        public Task<IEnumerable<AdministeredNutrient>> GetByPlant(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AdministeredNutrient> Get(Guid plantId, Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<AdministeredPlantNutrientsSummaryModel>> Sum(Guid plantId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
