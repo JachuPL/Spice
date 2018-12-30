7417a384caf6538f8fb0f4ffe69b5289763edb59
JachuPL <bonkowskijan@gmail.com>
2018-12-30 12:27:23 +0100

    Added stubs for command and query plant events implementations

---
+++ Application/Spice.Application/Plants/CommandPlantEvents.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Spice.Application.Common;
+using Spice.Application.Plants.Interfaces;
+using Spice.Application.Plants.Models;
+using Spice.Domain.Plants.Events;
+using System;
+using System.Threading.Tasks;
+
+namespace Spice.Application.Plants
+{
+    public class CommandPlantEvents : ICommandPlantEvents
+    {
+        private readonly IDatabaseService _database;
+        private readonly IMapper _mapper;
+
+        public CommandPlantEvents(IDatabaseService database, IMapper mapper)
+        {
+            _database = database;
+            _mapper = mapper;
+        }
+
+        public Task<Guid> Create(Guid plantId, CreatePlantEventModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Event> Update(Guid plantId, UpdatePlantEventModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(Guid plantId, Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Application/Spice.Application/Plants/QueryPlantEvents.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Spice.Application.Common;
+using Spice.Application.Plants.Interfaces;
+using Spice.Application.Plants.Models;
+using Spice.Domain.Plants.Events;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Spice.Application.Plants
+{
+    public class QueryPlantEvents : IQueryPlantEvents
+    {
+        private readonly IDatabaseService _database;
+        private readonly IMapper _mapper;
+
+        public QueryPlantEvents(IDatabaseService database, IMapper mapper)
+        {
+            _database = database;
+            _mapper = mapper;
+        }
+
+        public Task<IEnumerable<Event>> GetByPlant(Guid plantId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Event> Get(Guid plantId, Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<OccuredPlantEventsSummaryModel>> Sum(Guid plantId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
