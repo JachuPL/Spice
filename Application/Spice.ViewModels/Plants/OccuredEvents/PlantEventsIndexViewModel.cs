f8aab9386f6e400a47c7ab4c43f52ef6f6d3b67c
JachuPL <bonkowskijan@gmail.com>
2018-12-30 13:08:50 +0100

    Added Id field to index and details view models

--- Application/Spice.ViewModels/Plants/OccuredEvents/PlantEventsIndexViewModel.cs
+++ Application/Spice.ViewModels/Plants/OccuredEvents/PlantEventsIndexViewModel.cs
@@ -4,6 +4,7 @@ namespace Spice.ViewModels.Plants.OccuredEvents
 {
     public class PlantEventsIndexViewModel
     {
+        public Guid Id { get; set; }
         public EventTypeViewModel Type { get; set; }
         public DateTime Occured { get; set; }
     }
