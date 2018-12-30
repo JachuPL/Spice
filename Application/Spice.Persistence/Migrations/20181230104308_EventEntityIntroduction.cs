3623e0a18fbbec36c448d0d9cfa593fccf066c83
JachuPL <bonkowskijan@gmail.com>
2018-12-30 11:45:49 +0100

    Added new database migration to add plant events support

---
+++ Application/Spice.Persistence/Migrations/20181230104308_EventEntityIntroduction.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Spice.Persistence.Migrations
+{
+    public partial class EventEntityIntroduction : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    PlantId = table.Column<Guid>(nullable: false),
+                    Type = table.Column<int>(nullable: false),
+                    Description = table.Column<string>(maxLength: 500, nullable: false),
+                    Occured = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Events_Plants_PlantId",
+                        column: x => x.PlantId,
+                        principalTable: "Plants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_Id",
+                table: "Events",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_PlantId",
+                table: "Events",
+                column: "PlantId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Events");
+        }
+    }
+}
