a10da91942db082f7db9d429e85090b52ff2a401
JachuPL <bonkowskijan@gmail.com>
2018-12-30 10:25:22 +0100

    Added endpoint and view model factories and some integration tests

---
+++ Application/Tests/Spice.WebAPI.Tests/Plants/Factories/Events/ViewModelFactory.cs
@@ -0,0 +1,35 @@
+﻿using Spice.ViewModels.Plants.OccuredEvents;
+
+namespace Spice.WebAPI.Tests.Plants.Factories.Events
+{
+    internal static class ViewModelFactory
+    {
+        public static CreatePlantEventViewModel CreateValidCreationModel()
+        {
+            return new CreatePlantEventViewModel()
+            {
+            };
+        }
+
+        public static CreatePlantEventViewModel CreateInvalidCreationModel()
+        {
+            return new CreatePlantEventViewModel()
+            {
+            };
+        }
+
+        public static UpdatePlantEventViewModel CreateValidUpdateModel()
+        {
+            return new UpdatePlantEventViewModel()
+            {
+            };
+        }
+
+        public static UpdatePlantEventViewModel CreateInvalidUpdateModel()
+        {
+            return new UpdatePlantEventViewModel()
+            {
+            };
+        }
+    }
+}
