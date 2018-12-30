c0d3c69f636873751c1ab2fe299dd576ccc61ac4
JachuPL <bonkowskijan@gmail.com>
2018-12-30 10:39:19 +0100

    Added required view models and updated update event model

--- Application/Spice.Application/Plants/Models/UpdatePlantEventModel.cs
+++ Application/Spice.Application/Plants/Models/UpdatePlantEventModel.cs
@@ -1,6 +1,9 @@
-﻿namespace Spice.Application.Plants.Models
+﻿using System;
+
+namespace Spice.Application.Plants.Models
 {
     public class UpdatePlantEventModel
     {
+        public Guid Id { get; set; }
     }
 }
