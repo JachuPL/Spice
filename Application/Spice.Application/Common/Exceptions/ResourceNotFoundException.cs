24ff5501c001633af17a2cb5a73bb1cc0a6aa6a4
JachuPL <bonkowskijan@gmail.com>
2018-12-31 10:17:13 +0100

    Updated not found exception message

--- Application/Spice.Application/Common/Exceptions/ResourceNotFoundException.cs
+++ Application/Spice.Application/Common/Exceptions/ResourceNotFoundException.cs
@@ -4,7 +4,7 @@ namespace Spice.Application.Common.Exceptions
 {
     public abstract class ResourceNotFoundException : Exception
     {
-        protected ResourceNotFoundException(Type type, Guid id) : base($"No {type} by id {id}.")
+        protected ResourceNotFoundException(Type type, Guid id) : base($"{type.Name} with id {id} could not be found.")
         {
         }
     }
