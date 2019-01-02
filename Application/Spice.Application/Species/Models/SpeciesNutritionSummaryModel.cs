33608c7e508287d132028dc9a86d7de7f6842686
JachuPL <bonkowskijan@gmail.com>
2019-01-02 13:34:52 +0100

    Added fields to application and view models related to species nutrition summary

--- Application/Spice.Application/Species/Models/SpeciesNutritionSummaryModel.cs
+++ Application/Spice.Application/Species/Models/SpeciesNutritionSummaryModel.cs
@@ -1,6 +1,13 @@
-﻿namespace Spice.Application.Species.Models
+﻿using Spice.Application.Nutrients.Models;
+using System;
+
+namespace Spice.Application.Species.Models
 {
     public class SpeciesNutritionSummaryModel
     {
+        public NutrientDetailsModel Nutrient { get; set; }
+        public double TotalAmount { get; set; }
+        public DateTime FirstAdministration { get; set; }
+        public DateTime LastAdministration { get; set; }
     }
 }
--- Application/Spice.ViewModels/Species/SpeciesNutritionSummaryViewModel.cs
+++ Application/Spice.ViewModels/Species/SpeciesNutritionSummaryViewModel.cs
@@ -1,6 +1,13 @@
-﻿namespace Spice.ViewModels.Species
+﻿using Spice.ViewModels.Nutrients;
+using System;
+
+namespace Spice.ViewModels.Species
 {
     public class SpeciesNutritionSummaryViewModel
     {
+        public NutrientDetailsViewModel Nutrient { get; set; }
+        public double TotalAmount { get; set; }
+        public DateTime FirstAdministration { get; set; }
+        public DateTime LastAdministration { get; set; }
     }
 }
