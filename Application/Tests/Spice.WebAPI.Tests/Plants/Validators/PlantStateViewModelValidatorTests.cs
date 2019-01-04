0dc73e3799c5e00126074de49bd39a133b630711
JachuPL <bonkowskijan@gmail.com>
2019-01-04 10:43:01 +0100

    Added plant state view model validator and its tests

---
+++ Application/Tests/Spice.WebAPI.Tests/Plants/Validators/PlantStateViewModelValidatorTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using FluentValidation.Results;
+using NUnit.Framework;
+
+namespace Spice.ViewModels.Plants.Validators
+{
+    [TestFixture]
+    public class PlantStateViewModelValidatorTests
+    {
+        private PlantStateViewModelValidator _validator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _validator = new PlantStateViewModelValidator();
+        }
+
+        [TestCase(PlantStateViewModel.Healthy, TestName = "Update Plant Validator should not have error for Healty state")]
+        [TestCase(PlantStateViewModel.Deceased, TestName = "Update Plant Validator should not have error for Deceased state")]
+        [TestCase(PlantStateViewModel.Flowering, TestName = "Update Plant Validator should not have error for Flowering state")]
+        [TestCase(PlantStateViewModel.Fruiting, TestName = "Update Plant Validator should not have error for Fruiting state")]
+        [TestCase(PlantStateViewModel.Harvested, TestName = "Update Plant Validator should not have error for Harvested state")]
+        [TestCase(PlantStateViewModel.Sick, TestName = "Update Plant Validator should not have error for Sick state")]
+        public void ValidatorShouldNotHaveErrorWhenStateIsValid(PlantStateViewModel value)
+        {
+            ValidationResult result = _validator.Validate(value);
+            result.Errors.Should().BeEmpty();
+        }
+    }
+}
