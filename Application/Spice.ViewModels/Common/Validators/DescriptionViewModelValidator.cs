a49db990d21cb6124e8858a70b57ab705bb09f40
JachuPL <bonkowskijan@gmail.com>
2019-01-04 10:21:58 +0100

    Added description view model validator and its tests

---
+++ Application/Spice.ViewModels/Common/Validators/DescriptionViewModelValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Spice.ViewModels.Common.Validators
+{
+    public class DescriptionViewModelValidator : AbstractValidator<string>
+    {
+        public DescriptionViewModelValidator()
+        {
+            RuleFor(x => x).Must(BeAValidDescription)
+                           .WithMessage("Description cannot be build from whitespace characters only.");
+        }
+
+        private bool BeAValidDescription(string arg)
+        {
+            return string.IsNullOrEmpty(arg) || (arg.Trim().Length > 0);
+        }
+    }
+}
